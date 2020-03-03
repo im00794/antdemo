046022a07f135c70fdc609b7f4bea6ab6f7ec41c
imad <imadzafati@gmail.com>
2020-03-03 09:20:51 +0100

    ajout d'array

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,5 +4,12 @@ public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        System.out.println("hello");
+
+        String[] tech = new String[]{"mvn","ant","test"};
+
+        for (String str : tech) {
+            System.out.println("-->"+str);
+        }
     }
 }
