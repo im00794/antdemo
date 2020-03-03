1d1519686e7574442b52ec3146638b11ca9199f5
imad <imadzafati@gmail.com>
2020-03-03 10:36:00 +0100

    final

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,12 +1,16 @@
 package com.company;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
         System.out.println("hello");
 
-        String[] tech = new String[]{"one","two","three"};
+        List<String> tech = Arrays.asList(          new String[]{"one","two","three"};)
+
 
         for (String str : tech) {
             System.out.println("-->"+str+"!!!");
